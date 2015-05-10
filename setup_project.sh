bc3e55deceab611737e1352f2fe7c4bc136fed8c
JiM Strassburg <jstrassburg@gmail.com>
2015-05-09 22:32:47 -0500

    Damn copy-pasta, sooooo delicious.

--- setup_project.sh
+++ setup_project.sh
@@ -2,7 +2,7 @@
 
 #Check for pip
 echo "Checking for pip installation..."
-hash pip 2>/dev/null || { echo >&2 "pip is required to setup the virtualenv. Install pip and try again."; exit 1; }
+hash pip 2>/dev/null || { echo >&2 "pip is required to setup docker-compose. Install pip and try again."; exit 1; }
 
 pip install -r requirements.txt
 
